e411cecfd696c7bab58fae5b61bc9ff3d9aec585
Gabriella <gabbymay06@hotmail.com>
2018-05-23 14:55:49 -0400

    Started to make other activities for the camera and second screen once menu item is selected. Can click on start/stop button to reach screen where camera should be. Camera not implemented yet.

--- app/src/main/java/com/example/gabriella/hiowlstest/MainActivity.java
+++ app/src/main/java/com/example/gabriella/hiowlstest/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.gabriella.hiowlstest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public Button CamButton;
+
+    public void intit(){
+
+        CamButton=(Button)findViewById(R.id.CamButton);
+        CamButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent camera = new Intent(MainActivity.this, camera.class);
+
+                startActivity(camera);
+            }
+        });
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intit();
     }
 }
