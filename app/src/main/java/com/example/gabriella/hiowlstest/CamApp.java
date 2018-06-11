725d4a66e4211cf62126a1870e4efcb68ddbb70f
mayorgag4 <gabbymay06@hotmail.com>
2018-06-11 12:57:47 -0400

    Changed design and layout for image gallery instead of camera

--- app/src/main/java/com/example/gabriella/hiowlstest/CamApp.java
+++ app/src/main/java/com/example/gabriella/hiowlstest/CamApp.java
@@ -1,20 +1,61 @@
 package com.example.gabriella.hiowlstest;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class CamApp extends AppCompatActivity {
+    private static final int PICK_IMAGE = 100;
+    private ImageView imageView;
+    public Button pers, menuB;
 
-    public ImageButton pers;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cam_app);
+
+        imageView = (ImageView) findViewById(R.id.image_view);
+
+        Button pickImageButton = (Button) findViewById(R.id.pick_image_button);
+        pickImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+
+        infoPers();
+        menuButton();
+    }
+
+
+    private void openGallery() {
+        Intent gallery =
+                new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            Uri imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
+    }
 
 
     public void infoPers(){
 
-        pers=(ImageButton) findViewById(R.id.pers);
+        pers=(Button)findViewById(R.id.button3);
         pers.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,10 +66,17 @@ public class CamApp extends AppCompatActivity {
         });
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cam_app);
-        infoPers();
+
+    public void menuButton(){
+
+        menuB=(Button)findViewById(R.id.button4);
+        menuB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent menuBu = new Intent(CamApp.this, MainActivity.class);
+
+                startActivity(menuBu);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/gabriella/hiowlstest/MainActivity.java
+++ app/src/main/java/com/example/gabriella/hiowlstest/MainActivity.java
@@ -5,10 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
+
 
 public class MainActivity extends AppCompatActivity {
-
 
     public Button CamButton;
 
